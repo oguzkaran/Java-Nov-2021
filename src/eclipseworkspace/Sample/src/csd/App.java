179e1e92e988cf479d3b7c44eed1f65a61be33b7
Oğuz Karan <oguzkaran@csystem.org>
2021-11-28 16:46:18 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,28 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-     Tür (type): Bir değişken için bellekte ne kadar yer ayrılacağını ve içerisindeki bilginin hangi formatta tutulacağını 
-     belirten kavrama denir.
-     
-     Java'da türler genel olarak  temel türler (primitive/built-in/predefined types) ve programcının tanımladığı 
-     türler (user defined types) olmak üzere iki gruba ayrılır. 
-     
-     Temel türler:
-     Tür İsmi			Uzunluğu (byte)
-     short					2
-     int					4
-     long					8
-     byte					1
-     
-     double					8
-     float					4
-     
-     char					2
-     boolean				-     
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{		
+	{
 		
 	}
 }
