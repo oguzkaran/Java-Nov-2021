c09f981bffcc2b6aaa704f4c9a9ad2d2b30fc772
Oğuz Karan <oguzkaran@csystem.org>
2021-11-28 14:50:41 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,43 +1,29 @@
 /*----------------------------------------------------------------------------------------------------------------------
-     Metot çağırmanın genel biçimi:
-     [paket ismi].[sınıf ismi].<metot ismi>([argümanlar]);
+     Tür (type): Bir değişken için bellekte ne kadar yer ayrılacağını ve içerisindeki bilginin hangi formatta tutulacağını 
+     belirten kavrama denir.
      
-     Anahtar Notlar: println de bir metottur. Fakat System bir paket ismi değildir, out da bir sınıf ismi değildir.
-     Bu çağrının nasıl yapıldığı ileride ele alınacaktır.
+     Java'da türler genel olarak  temel türler (primitive/built-in/predefined types) ve programcının tanımladığı 
+     türler (user defined types) olmak üzere iki gruba ayrılır. 
      
-     main metoduna "entry point" denir. main metodu java programı tarafından yani JVM tarafından çağrılır. main
-     metodu bittiğinde program akış sonlanır 
+     Temel türler:
+     Tür İsmi			Uzunluğu (byte)
+     short					2
+     int					4
+     long					8
+     byte					1
+     
+     double					8
+     float					4
+     
+     char					2
+     boolean				-     
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		System.out.println("Hello, World");
-		csd.Sample.foo();	
-		csd.Sample.bar();
-		csd.Mample.tar();
-		System.out.println("Goodbye, World");
+		
 	}
 }
 
-class Sample {
-	public static void foo()
-	{
-		System.out.println("Sample -> foo");
-	}
-	
-	public static void bar()
-	{
-		System.out.println("Sample -> bar");
-		csd.Mample.tar();
-	}
-}
-
-
-class Mample {
-	public static void tar()
-	{
-		System.out.println("Mample -> tar");
-	}
-}
