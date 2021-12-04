89f02c75b3c4a1b73ffe24decfdc7eec185354f7
Oğuz Karan <oguzkaran@csystem.org>
2021-12-04 14:37:53 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,12 +1,23 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	
+	Klavyeden int türden değerler okunması
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.print("Birinci sayı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());
+		
+		System.out.print("İkinci sayı giriniz:");
+		int b = Integer.parseInt(kb.nextLine());
+		
+		int total = a + b;
+		
+		System.out.println(total);		
 	}
 }
+
 
