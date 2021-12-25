07803c685e35decbf9354a52e081a62400e652ec
Oğuz Karan <oguzkaran@csystem.org>
2021-12-25 16:41:23 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,34 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden 3(üç) basamaklı bir sayının basamakları toplamını döngü kullanmadan
-	döndüren digitsSum metodunu yazınız ve aşağıdaki kod ile test ediniz
-	Açıklamalar:	 
-		- Metot basamak sayısı kontrolü yapmayacaktır
-		- Metot negatif sayılar için basamakları toplamını pozitif olarak döndürecektir 
+	! operatörü sağdan sola (left associative) önceliklidir. Aşağdıdaki durumu açıklamak için yazılmıştır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		DigitsSumTest.run();
-	}
-}
-
-class DigitsSumTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int val = Integer.parseInt(kb.nextLine());
-		
-		System.out.printf("%d sayısının basamakları toplamı:%d%n", val, Util.digitsSum(val));
-	}
-}
-
-class Util {
-	public static int digitsSum(int val)
-	{				
-		return Math.abs(val / 100 + val / 10 % 10 + val % 10);
+		int a = - - - - - - -2;
 	}
 }
 
