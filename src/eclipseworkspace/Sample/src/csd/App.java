5126939f93b8ce0b79e8c39aed4f61efd0331921
Oğuz Karan <oguzkaran@csystem.org>
2021-12-26 14:36:20 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,12 +1,37 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	! operatörü sağdan sola (left associative) önceliklidir. Aşağdıdaki durumu açıklamak için yazılmıştır
+	Bileşik deyim
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		int a = - - - - - - -2;
+		int a = 10, b = 20;;;;;;;;;;;;	//Bildirim deyimi	
+		
+		{ //Bileşik deyim
+			++a; //basit deyim
+			b--; //basit deyim
+			Sample.foo(); //basit deyim
+			
+			{
+				System.out.printf("a = %d%n", a);
+				System.out.printf("b = %d%n", b);
+			}
+			//...
+		}
+		;;;;;;;;;;;;;;;
+		
+		; //boş deyim
+		
+		;
+		
+		;
 	}
 }
 
+class Sample {
+	public static void foo()
+	{
+		System.out.println("foo");		
+	}
+}
