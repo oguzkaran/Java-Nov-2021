5e02fe656d2bb4db80d8997a9051f65901edf282
Oğuz Karan <oguzkaran@csystem.org>
2022-01-02 14:43:18 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,31 +1,63 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	boolean türden geri dönüş değeri olan bir metodun if deyiminin koşul ifadesinde çağrılması
+	Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden bir denklemin köklerini bulup ekrana yazdıran
+	programı yazınız
+	(İleride daha iyisi yazılacaktır)
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);		
-		System.out.print("Bir sayı giriniz:");
-		int val = Integer.parseInt(kb.nextLine());
-		
-		if (!NumberUtil.isEven(val))
-			System.out.println("Tek");
-		else
-			System.out.println("Çift");
-			
-		System.out.println("Tekrar yapıyor musunuz?");		
+		EquationSolverApp.run();		
 	}
 }
 
-
-class NumberUtil {
-	public static boolean isEven(int val)
+class EquationSolverApp {
+	public static void run()
 	{
-		return val % 2 == 0;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("a?");
+		double a = Double.parseDouble(kb.nextLine());
+		
+		System.out.print("b?");
+		double b = Double.parseDouble(kb.nextLine());
+		
+		System.out.print("c?");
+		double c = Double.parseDouble(kb.nextLine());
+		
+		EquationSolver.findRoots(a, b, c);
+	}
+}
+
+class EquationSolver {
+	public static double getDelta(double a, double b, double c)
+	{
+		return b * b - 4 * a * c;
 	}
 	
-	//...
+	public static void doWorkForPositiveDelta(double delta, double a, double b)
+	{
+		double sqrtDelta = Math.sqrt(delta);
+		double x1 = (-b + sqrtDelta) / (2 * a);
+		double x2 = (-b - sqrtDelta) / (2 * a);
+		
+		System.out.printf("x1 = %f, x2 = %f%n", x1, x2);		
+	}
+	
+	public static void doWorkForZeroDelta(double delta, double a, double b)
+	{		
+		System.out.printf("x1 = x2 = %f%n", -b / (2 * a));
+	}
+	
+	public static void findRoots(double a, double b, double c)
+	{
+		double delta = getDelta(a, b, c);
+		
+		if (delta > 0)
+			doWorkForPositiveDelta(delta, a, b);		
+		else if (delta == 0) 
+			doWorkForZeroDelta(delta, a, b);
+		else
+			System.out.println("Gerçek kök yok");
+	}
 }
-
