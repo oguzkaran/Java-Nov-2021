6f9740d72606d81ae55aab5cb7a6dc69f12ce587
Oğuz Karan <oguzkaran@csystem.org>
2022-01-08 17:32:55 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,38 +1,13 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının basamak sayısını döndüren digitsCount isimli
-	metodu yazınız ve aşağıdaki kod ile test ediniz 	
+	Aşağıdaki örnekte error oluşur
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{
-		DigitsCountTest.run();			
+	{		
+		for (int i = 0; i < 10; ++i);
+			System.out.printf("%d ", i); //error
 	}
 }
 
-class DigitsCountTest {
-	
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.println("Sayıları girmeye başlayınız:");
-		
-		while (true) {
-			System.out.print("Bir sayı giriniz:");
-			int val = Integer.parseInt(kb.nextLine());
-			
-			System.out.printf("%d sayısının basamak sayısı:%d%n", val, NumberUtil.digitsCount(val));
-			
-			if (val == 0)
-				return;
-		}
-	}
-}
-
-class NumberUtil {
-	public static int digitsCount(int val)
-	{
-		//TODO:
-	}
-}
