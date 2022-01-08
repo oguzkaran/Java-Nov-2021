0172fdf3b01851cdb6effede43e578c32b569914
Oğuz Karan <oguzkaran@csystem.org>
2022-01-08 14:56:09 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,47 +1,38 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan sayıların pozitif ve negatif olanlarının ayrı ayrı toplamlarını
-	bulan programı yazınız 	
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının basamak sayısını döndüren digitsCount isimli
+	metodu yazınız ve aşağıdaki kod ile test ediniz 	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		PosNegSumSolver.run();			
+		DigitsCountTest.run();			
 	}
 }
 
-class PosNegSumSolver {
-	public static void displayResult(int sumPos, int sumNeg)
-	{
-		if (sumPos != 0)
-			System.out.printf("Pozitif sayıların toplamı:%d%n", sumPos);
-		else
-			System.out.println("Hiç pozitif sayı girmediniz");
-		
-		if (sumNeg != 0)
-			System.out.printf("Negatif sayıların toplamı:%d%n", sumNeg);
-		else
-			System.out.println("Hiç negatif sayı girmediniz");
-	}
+class DigitsCountTest {
 	
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		System.out.println("Sayıları girmeye başlayınız:");
 		
-		int val;
-		int sumPos = 0, sumNeg = 0;
-		
-		System.out.print("Bir sayı giriniz:");
-		while ((val = Integer.parseInt(kb.nextLine())) != 0) {
-			if (val > 0)
-				sumPos += val;
-			else
-				sumNeg += val;
+		while (true) {
 			System.out.print("Bir sayı giriniz:");
+			int val = Integer.parseInt(kb.nextLine());
+			
+			System.out.printf("%d sayısının basamak sayısı:%d%n", val, NumberUtil.digitsCount(val));
+			
+			if (val == 0)
+				return;
 		}
-		
-		displayResult(sumPos, sumNeg);
+	}
+}
+
+class NumberUtil {
+	public static int digitsCount(int val)
+	{
+		//TODO:
 	}
 }
