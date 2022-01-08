4eb196de7c849774ed9d9dc0b83d74bad3038ed9
Oğuz Karan <oguzkaran@csystem.org>
2022-01-08 14:30:36 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,31 +1,47 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örnekte return deyimiile sonsuz döngüden de çıkılmış olur 	
+	Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan sayıların pozitif ve negatif olanlarının ayrı ayrı toplamlarını
+	bulan programı yazınız 	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		System.out.printf("Toplam:%d%n", Util.getTotal());				
+		PosNegSumSolver.run();			
 	}
 }
 
-class Util {
-	public static int getTotal()
+class PosNegSumSolver {
+	public static void displayResult(int sumPos, int sumNeg)
+	{
+		if (sumPos != 0)
+			System.out.printf("Pozitif sayıların toplamı:%d%n", sumPos);
+		else
+			System.out.println("Hiç pozitif sayı girmediniz");
+		
+		if (sumNeg != 0)
+			System.out.printf("Negatif sayıların toplamı:%d%n", sumNeg);
+		else
+			System.out.println("Hiç negatif sayı girmediniz");
+	}
+	
+	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		System.out.println("Sayıları girmeye başlayınız:");
 		
 		int val;
-		int sum = 0;
+		int sumPos = 0, sumNeg = 0;
 		
-		while (true) {
-			val = Integer.parseInt(kb.nextLine());
-			
-			if (val == 0)
-				return sum;
-			
-			sum += val;
-		}		
+		System.out.print("Bir sayı giriniz:");
+		while ((val = Integer.parseInt(kb.nextLine())) != 0) {
+			if (val > 0)
+				sumPos += val;
+			else
+				sumNeg += val;
+			System.out.print("Bir sayı giriniz:");
+		}
+		
+		displayResult(sumPos, sumNeg);
 	}
 }
