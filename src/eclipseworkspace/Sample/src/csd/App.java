d5b89b9a9085db79fd9bb4968bcf0a6b1788c7f7
Oğuz Karan <oguzkaran@csystem.org>
2022-01-29 16:39:14 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,44 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örnekte switch expression'ın default bölümü olmalıdır. Çünkü switch expression'ın ürettiği değer 
-	kullanılmıştır
+	char türünden short türüne doğrudan dönüşüm geçersizdir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
+		char c = 10;
+		short b;
 		
-		for (;;) {
-			System.out.print("Telefon kodunu giriniz:");
-			int code = Integer.parseInt(kb.nextLine());
-			
-			if (code <= 0)
-				break;
-			
-			int plate = switch (code) { //error
-				case 212:
-				case 216:
-					System.out.println("İstanbul");
-					yield 34;				
-				case 372:
-					yield 67;
-				case 312:
-					yield 6;
-				case 232:
-					yield 35;
-				default:
-					yield -1;
-			};
-			
-			
-			if (plate != -1)
-				System.out.printf("Plaka:%d%n", plate);
-			else
-				System.out.println("Geçersiz plaka");
-		}
-		
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		b = c; //error		
 	}
 }
+
