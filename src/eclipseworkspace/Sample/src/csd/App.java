79b8cf1438849e680f0f3f12d8f58a49f2f1a53a
Oğuz Karan <oguzkaran@csystem.org>
2022-01-30 16:26:28 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,19 +1,22 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Küçük tamsayı türü ile büyük tamsayı türü işleme sokulduğunda dönüşüm büyük tamsayı türüne doğrudur. Sonuç büyük
-	tamsayı türünden çıkar. "Integral promotion" bu kuralın dışındadır
+	Gerçek sayı türünden tamsayı türüne yapılan explicit dönüşüm şu aşamalarla gerçekleşir:
+	- Sayının noktadan sonraki kısmı atılır
+		- Elde edilen değer hedef türün sınırları içerisinde kalıyorsa doğrudan hedef türe atanır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{
-		short a = 10;
-		long b = 34;
-		long c;
+	{	
+		double a;
+		short b;
 		
-		c = a + b;
+		a = 5_000_000_000.56;
+		b = (short)a;
 		
-		System.out.printf("c = %d%n", c);
+		System.out.printf("5000000000 = %d%n", (short)5_000_000_000L);
+		System.out.printf("2147483648 = %d%n", (short)2147483647);
+		System.out.printf("b = %d%n", b);				
 	}
 }
 
