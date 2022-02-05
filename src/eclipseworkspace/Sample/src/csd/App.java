3907a95e390999f5b376af00feb960c3a4a5c2f3
Oğuz Karan <oguzkaran@csystem.org>
2022-02-05 16:40:16 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -7,37 +7,7 @@ package csd;
 class App {
 	public static void main(String [] args) 
 	{
-		GcdTest.run();									
-	}
-}
-
-class GcdTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		for (;;) {
-			System.out.print("Birinci sayıyı giriniz:");
-			int a = Integer.parseInt(kb.nextLine());
-			
-			System.out.print("İkinci sayıyı giriniz:");
-			int b = Integer.parseInt(kb.nextLine());
-			
-			int result = NumberUtil.gcd(a, b);
-			
-			System.out.printf("ebob(%d, %d) = %d%n", a, b, result);
-			
-			if (result == 1)
-				break;
-		}
-	}
-}
-
-
-class NumberUtil {
-	public static int gcd(int a, int b)
-	{
-		//TODO:
+											
 	}
 }
 
