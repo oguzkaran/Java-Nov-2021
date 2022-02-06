ca51d6a7d9258f9db9ea00b5d9e8aedd0ab7d904
Oğuz Karan <oguzkaran@csystem.org>
2022-02-06 16:05:39 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,72 +1,26 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Homework-002-5. sorunun bir çözümü
-	(Not: Çözüm çalışma sorusunun verildiği tarihte işlenmiş olan konulara göre yazılmıştır)
+	Yukarıdaki örnek için gerek olmasa da parantez konması okunabilirliği artırır 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		PrintGolbachPrimesApp.run();											
-	}
-}
-
-class PrintGolbachPrimesApp {
-	public static void run()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Birinci sayıyı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());
+
+		System.out.print("İkinci sayıyı giriniz:");
+		int b = Integer.parseInt(kb.nextLine());
 		
-		for (;;) {
-			System.out.print("2'den büyük bir çift sayı giriniz:?");
-			int val = Integer.parseInt(kb.nextLine());
-			
-			if (val == 0)
-				break;			
-			
-			if (val <= 2 || val % 2 != 0) {
-				System.out.println("2'den büyük çift sayı girmelisiniz!...");
-				continue;
-			}			
-			
-			NumberUtil.printGoldBachPrimes(val);						
-		}
+		System.out.print("Üçüncü sayıyı giriniz:");
+		int c = Integer.parseInt(kb.nextLine());
 		
-		System.out.println("Tekrar yapıyor musunuz?");
+		int max;		
+		
+		max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+		
+		System.out.printf("max(%d, %d, %d) = %d%n", a, b, c, max);
 	}
 }
 
-class NumberUtil {
-	public static void printGoldBachPrimes(int val)
-	{
-		for (int a = 2; a < val; ++a) {
-			int b = val - a;
-			
-			if (isPrime(a) && isPrime(b) && a <= b)
-				System.out.printf("%d + %d = %d == %d%n", a, b, a + b, val);
-		}		
-	}
-	
-	public static boolean isPrime(long val)
-	{
-		if (val <= 1)
-			return false;
-		
-		if (val % 2 == 0)
-			return val == 2;
-		
-		if (val % 3 == 0)
-			return val == 3;
-		
-		if (val % 5 == 0)
-			return val == 5;
-		
-		if (val % 7 == 0)
-			return val == 7;	
-		
-		for (long i = 11; i * i <= val; i += 2)		
-			if (val % i == 0)
-				return false;		
-		
-		return true;							
-	}	
-}
