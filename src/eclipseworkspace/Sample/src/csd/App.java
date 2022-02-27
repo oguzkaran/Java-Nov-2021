76bb16f758db03504820ba05b36b877156531e2d
Oğuz Karan <oguzkaran@csystem.org>
2022-02-27 15:39:53 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,63 +1,31 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	Homework-003-8. sorunun bir çözümü
-	(Not: Çözüm çalışma sorusunun verildiği tarihte işlenmiş olan konulara göre yazılmıştır)
-	Açıklama: Soruda ikinci bir Scanner nesnesi yaratılmaması için bir çok işlem metot olarak yazılmamıştır
+/*----------------------------------------------------------------------------------------------------------------------	
+	Her new işlemi yeni bir nesne yaratmak olduğuna göre aşağıdaki örnekte ** ile belirtilen deyimde yeni bir nesne 
+	yaratılmış ve adresi s referansına atanmıştır. Bu durumda artık s yeni bir nesneyi gösteriyor duruma gelmiştir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{
-		CalculateSumCountMinMaxApp.run();		
+	{		
+		Sample s;	
+		
+		s = new Sample();	
+		
+		s.x = 10;
+		s.y = true;		
+		
+		System.out.printf("s.x = %d%n", s.x);
+		System.out.printf("s.y = %b%n", s.y);
+		
+		s = new Sample(); //**
+		
+		System.out.printf("s.x = %d%n", s.x);
+		System.out.printf("s.y = %b%n", s.y);		
 	}
 }
 
-class CalculateSumCountMinMaxApp {
-	public static void displayReport(int count, int sum, int min, int max)
-	{
-		System.out.printf("Toplam %d değer girildi%n", count);
-		System.out.printf("Max%d%n", max);
-		System.out.printf("Min:%d%n", min);
-		System.out.printf("Ortalama:%f%n", (double)sum / count);
-	}
-	
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);		
-		int count = 0;
-		int sum = 0;
-		int minVal = 100;
-		int maxVal = 0;
-		
-		for (;;) {
-			int val;
-			
-			for (;;) {
-				System.out.print("Bir tamsayı giriniz:");
-				val = Integer.parseInt(kb.nextLine());
-				
-				if (0 <= val && val <= 100)
-					break;
-				
-				System.out.print("Geçersiz değer girdiniz!...Yeni bir değer giriniz:");
-			}
-			
-			++count;
-			sum += val;
-			minVal = Math.min(minVal, val);
-			maxVal = Math.max(maxVal, val);
-			
-			System.out.print("Yeni bir değer girmek istiyor musunuz? [Evet için 1(bir) Hayır için 0(sıfır) değerlerinden birini giriniz]:");
-			
-			int option;
-			
-			do 
-				option = Integer.parseInt(kb.nextLine());
-			while (option != 0 && option != 1);	
-			
-			if (option == 0)
-				break;
-		}
-		displayReport(count, sum, minVal, maxVal);
-	}
+class Sample {
+	public int x;
+	public boolean y;
+	//...
 }
