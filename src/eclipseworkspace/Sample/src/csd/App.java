90e0328bb32d049037aedc5a5eb9e0e37bbd77a7
Oğuz Karan <oguzkaran@csystem.org>
2022-03-05 16:47:05 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,86 +1,69 @@
-/*----------------------------------------------------------------------------------------------------------------------	
-	Yukarıdaki örnekte anlatılan problemler aşağıdkai gibi static veri elemanları kullanılarak çözülebilir. Böylece
-	kod karmaşası da azalmış olur. Örnekte yine detaylar gözardı edilmiştir. Burada anlatılmak istenen konulara odaklamanız
-	tavsiye edilir. Ayrıca ** ile belirtilen kod parçaları konu açısından önemsizdir
+/*----------------------------------------------------------------------------------------------------------------------
+	Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden bir denklemin köklerini bulup ekrana yazdıran
+	programı yazınız
+
+	Sınıfın aşağıdaki kullanım şekli sınıfların yararı 100 birim düşünülürse %1 civarındadır	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
-		WarGameApp.run();
-		
+	{	
+		EquationSolverApp.run();		
 	}
 }
 
-class WarGameApp {
-	//...	
-	
+class EquationSolverApp {
 	public static void run()
 	{
-		//...		
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("a?");
+		double a = Double.parseDouble(kb.nextLine());
 		
-		for (int i = 0; i < 10; ++i) {
-			Alien a;
+		System.out.print("b?");
+		double b = Double.parseDouble(kb.nextLine());
+		
+		System.out.print("c?");
+		double c = Double.parseDouble(kb.nextLine());
+		
+		RootInfo ri = EquationSolver.findRoots(a, b, c);
+		
+		if (ri.exists)
+			System.out.printf("x1 = %f, x2 = %f%n", ri.x1, ri.x2);
+		else
+			System.out.println("Gerçek kök yok");
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}	
+}
+
+class RootInfo {
+	public double x1;
+	public double x2;
+	public boolean exists;
+}
+
+class EquationSolver {
+	public static double getDelta(double a, double b, double c)
+	{
+		return b * b - 4 * a * c;
+	}	
+	
+	public static RootInfo findRoots(double a, double b, double c)
+	{
+		double delta = getDelta(a, b, c);
+		RootInfo result;
+		
+		result = new RootInfo();
+		
+		if (delta >= 0) {
+			double sqrtDelta = Math.sqrt(delta);
 			
-			a = new Alien();
-			
-			//...		
+			result.x1 = (-b + sqrtDelta) / (2 * a);
+			result.x2 = (-b - sqrtDelta) / (2 * a);
+			result.exists = true;
 		}
 		
-		//...
-		
-		for (int i = 0; i < 20; ++i) {
-			Soldier s;
-			
-			s = new Soldier();			
-			//...			
-		}	
-		
-		//...
-		
-		System.out.printf("Number of aliens:%d%n", Alien.count);
-		System.out.printf("Number of soldiers:%d%n", Soldier.count);
-		
-		//...
+		return result;
 	}
-} 
-
-class Alien {
-	public static int count;
-	public int numberOfArms;
-	public int color;
-	
-	public Alien() //**
-	{
-		++count;
-	}
-	//...
-}
-
-class Soldier {
-	public static int count;
-	public int title;
-	public int numberOfGuns;
-	
-	public Soldier() //**
-	{
-		++count;
-	}
-	//...
-}
-
-class Civilian {
-	public static int count;
-	//...
-}
-
-class Animal {
-	public static int count;
-	//...
-}
-
-class Building {
-	public static int count;
-	//...
 }
