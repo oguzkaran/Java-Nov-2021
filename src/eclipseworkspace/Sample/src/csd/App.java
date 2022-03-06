6be8001e2a06f39ac66c234141cac227947be50c
Oğuz Karan <oguzkaran@csystem.org>
2022-03-06 17:05:57 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,91 +1,55 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Point sınıfı 	
+	Sınıf Çalışması: İki zar atıldığında zarların çift gelmesi (ikisi de aynı) olasılığını yaklaşık olarak hesaplayan
+	simülasyon programını yazınız
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		Point p1, p2;
-		
-		p1 = new Point();
-		p2 = new Point();
-		
-		System.out.print("Bir sayı giriniz:");
-		int count = Integer.parseInt(kb.nextLine());
-		
-		for (int i = 0; i < count; ++i) {
-			System.out.print("Birinci nokta için x?");
-			p1.x = Double.parseDouble(kb.nextLine());
-			
-			System.out.print("Birinci nokta için y?");
-			p1.y = Double.parseDouble(kb.nextLine());
-			
-			System.out.print("İkinci nokta için x?");
-			p2.x = Double.parseDouble(kb.nextLine());
-			
-			System.out.print("İkinci nokta için y?");
-			p2.y = Double.parseDouble(kb.nextLine());
-			
-			System.out.println("------------------------");
-			
-			System.out.printf("Distance:%f%n", p1.distance(p2));
-			
-			System.out.println("------------------------");
-		}
-		
-		System.out.println("------------------------");
-		System.out.print("İkinci nokta için x?");
-		double x = Double.parseDouble(kb.nextLine());
-		
-		System.out.print("İkinci nokta için y?");
-		double y = Double.parseDouble(kb.nextLine());	
-		
-		
-		System.out.printf("Distance:%f%n", p1.distance(x, y));
-		
-		System.out.println("------------------------");
-		
-		System.out.printf("Distance:%f%n", p1.distance());		
+		CoinSimulationApp.run();
 	}
 }
 
-class Point {
-	 public double x;
-	 public double y;
-	 
-	 //...	 
-	 
-	 public double distance()
-	 {
-		 return distance(0, 0);		 		 
-	 }
-	 
-	 public double distance(Point other)
-	 {
-		 return distance(other.x, other.y);
-	 }
-	 
-	 public double distance(double a, double b)
-	 {
-		 return Math.sqrt(Math.pow(x - a, 2) + Math.pow(y - b, 2));		 		 		 		 
-	 }
-	 
-	 public void offset(double dxy)
-	 {
-		 offset(dxy, dxy);
-	 }
-	 
-	 public void offset(double dx, double dy)
-	 {
-		 x += dx;
-		 y += dy;
-	 }	 
-	 
-	 public void print() 
-	 {
-		 System.out.printf("(%f, %f)%n", x, y);
-	 }
+class CoinSimulationApp {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		CoinSimulation cs = new CoinSimulation();
+		
+		for (;;) {
+			System.out.print("Parayı kaç kez atmak istiyorsunuz?");
+			int count = Integer.parseInt(kb.nextLine());
+			
+			if (count <= 0)
+				break;
+			
+			cs.run(count);
+			System.out.printf("Yazı gelme olasılığı:%f%n", cs.p);
+		}
+				
+	}
+}
+
+class CoinSimulation {
+	public double p;
+	
+	//...
+	public static int getNumberOfTails(int count)
+	{		
+		java.util.Random r = new java.util.Random();
+		int n = 0;
+		
+		for (int i = 0; i < count; ++i)
+			if (r.nextBoolean())
+				++n;
+		
+		return n;
+	}
+	
+	
+	public void run(int count)
+	{
+		p = getNumberOfTails(count) / (double)count;				
+	}
 }
