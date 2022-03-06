2cb710e73f686710b96f4300e574f00c7c15f4df
Oğuz Karan <oguzkaran@csystem.org>
2022-03-06 14:26:18 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,69 +1,91 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden bir denklemin köklerini bulup ekrana yazdıran
-	programı yazınız
-
-	Sınıfın aşağıdaki kullanım şekli sınıfların yararı 100 birim düşünülürse %1 civarındadır	
+	Point sınıfı 	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		EquationSolverApp.run();		
-	}
-}
-
-class EquationSolverApp {
-	public static void run()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("a?");
-		double a = Double.parseDouble(kb.nextLine());
 		
-		System.out.print("b?");
-		double b = Double.parseDouble(kb.nextLine());
+		Point p1, p2;
 		
-		System.out.print("c?");
-		double c = Double.parseDouble(kb.nextLine());
+		p1 = new Point();
+		p2 = new Point();
 		
-		RootInfo ri = EquationSolver.findRoots(a, b, c);
+		System.out.print("Bir sayı giriniz:");
+		int count = Integer.parseInt(kb.nextLine());
 		
-		if (ri.exists)
-			System.out.printf("x1 = %f, x2 = %f%n", ri.x1, ri.x2);
-		else
-			System.out.println("Gerçek kök yok");
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}	
-}
-
-class RootInfo {
-	public double x1;
-	public double x2;
-	public boolean exists;
-}
-
-class EquationSolver {
-	public static double getDelta(double a, double b, double c)
-	{
-		return b * b - 4 * a * c;
-	}	
-	
-	public static RootInfo findRoots(double a, double b, double c)
-	{
-		double delta = getDelta(a, b, c);
-		RootInfo result;
-		
-		result = new RootInfo();
-		
-		if (delta >= 0) {
-			double sqrtDelta = Math.sqrt(delta);
+		for (int i = 0; i < count; ++i) {
+			System.out.print("Birinci nokta için x?");
+			p1.x = Double.parseDouble(kb.nextLine());
 			
-			result.x1 = (-b + sqrtDelta) / (2 * a);
-			result.x2 = (-b - sqrtDelta) / (2 * a);
-			result.exists = true;
+			System.out.print("Birinci nokta için y?");
+			p1.y = Double.parseDouble(kb.nextLine());
+			
+			System.out.print("İkinci nokta için x?");
+			p2.x = Double.parseDouble(kb.nextLine());
+			
+			System.out.print("İkinci nokta için y?");
+			p2.y = Double.parseDouble(kb.nextLine());
+			
+			System.out.println("------------------------");
+			
+			System.out.printf("Distance:%f%n", p1.distance(p2));
+			
+			System.out.println("------------------------");
 		}
 		
-		return result;
+		System.out.println("------------------------");
+		System.out.print("İkinci nokta için x?");
+		double x = Double.parseDouble(kb.nextLine());
+		
+		System.out.print("İkinci nokta için y?");
+		double y = Double.parseDouble(kb.nextLine());	
+		
+		
+		System.out.printf("Distance:%f%n", p1.distance(x, y));
+		
+		System.out.println("------------------------");
+		
+		System.out.printf("Distance:%f%n", p1.distance());		
 	}
+}
+
+class Point {
+	 public double x;
+	 public double y;
+	 
+	 //...	 
+	 
+	 public double distance()
+	 {
+		 return distance(0, 0);		 		 
+	 }
+	 
+	 public double distance(Point other)
+	 {
+		 return distance(other.x, other.y);
+	 }
+	 
+	 public double distance(double a, double b)
+	 {
+		 return Math.sqrt(Math.pow(x - a, 2) + Math.pow(y - b, 2));		 		 		 		 
+	 }
+	 
+	 public void offset(double dxy)
+	 {
+		 offset(dxy, dxy);
+	 }
+	 
+	 public void offset(double dx, double dy)
+	 {
+		 x += dx;
+		 y += dy;
+	 }	 
+	 
+	 public void print() 
+	 {
+		 System.out.printf("(%f, %f)%n", x, y);
+	 }
 }
