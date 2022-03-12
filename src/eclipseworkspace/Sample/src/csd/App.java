414002c47f2ce9849419c07e6b6333409ff5efb5
Oğuz Karan <oguzkaran@csystem.org>
2022-03-12 14:31:17 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,55 +1,53 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: İki zar atıldığında zarların çift gelmesi (ikisi de aynı) olasılığını yaklaşık olarak hesaplayan
-	simülasyon programını yazınız
+	ctor aşağıdaki özelliklere sahip bir metottur:
+	- Sınıfı yazan programcı sınıfa hiçbir ctor yazmazsa parametresiz ctor'u (default ctor) derleyici tarafından içi boş
+	ve public olarak yazılır
+	
+	- Sınıfı yazan programcı en az bir tane ctor yazarsa default ctor derleyici tarafından yazılmaz. Artık default ctor
+	yazılıp yazılmayacağı sınıfı yazan programcıya bırakılmış olur
+	
+	- ctor sınıf ismi ile aynı olan ve geri dönüş değeri kavramı olmayan bir metottur. Geri dönüş değeri bilgisi yazılmaz.
+	void da yazılmaz. Sınıf ismi ile aynı isimde bir metot yazılıp geri dönüş değeri bilgisi de yazılırsa artık bu metot
+	ctor olmaz. Şüphesiz böyle bir metot yazılmamalıdır  
+	 
+	- ctor overload edilebilir (constructor overloading)
+	
+	- ctor non-static bir metottur
+	
+	- 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		CoinSimulationApp.run();
-	}
-}
-
-class CoinSimulationApp {
-	public static void run()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		CoinSimulation cs = new CoinSimulation();
+		System.out.print("Minimum değer giriniz:");
+		int min = Integer.parseInt(kb.nextLine());
 		
-		for (;;) {
-			System.out.print("Parayı kaç kez atmak istiyorsunuz?");
-			int count = Integer.parseInt(kb.nextLine());
-			
-			if (count <= 0)
-				break;
-			
-			cs.run(count);
-			System.out.printf("Yazı gelme olasılığı:%f%n", cs.p);
-		}
-				
-	}
-}
-
-class CoinSimulation {
-	public double p;
-	
-	//...
-	public static int getNumberOfTails(int count)
-	{		
-		java.util.Random r = new java.util.Random();
-		int n = 0;
+		System.out.print("Maksimum değer giriniz:");
+		int max = Integer.parseInt(kb.nextLine());
+		
+		System.out.print("Kaç tane sayı üretmek istersiniz?");
+		int count = Integer.parseInt(kb.nextLine());
 		
 		for (int i = 0; i < count; ++i)
-			if (r.nextBoolean())
-				++n;
+			System.out.printf("%d ", (int)(Math.random() * (max - min) + min)); //[min, max)
 		
-		return n;
-	}
-	
-	
-	public void run(int count)
-	{
-		p = getNumberOfTails(count) / (double)count;				
+		System.out.println();
 	}
 }
+
+class Sample {
+	public Sample()
+	{
+		
+	}
+	
+	public Sample(int a)
+	{
+		
+	}
+}
+
+
