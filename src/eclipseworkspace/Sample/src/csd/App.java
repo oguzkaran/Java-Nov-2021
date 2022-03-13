2e2fc38e58cd5c045dcc6abbfccec3d9d8a0b43f
Oğuz Karan <oguzkaran@csystem.org>
2022-03-13 16:40:22 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,22 +1,35 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Derleyici bir "String literal" gördüğünde şöyle bir kod üretir: "Çalışma zamanında String türden bir nesne yarat, 
-	karakterleri bu nesneye aktar ve o nesnenin adresini (referansını) ver"
+	Sınıf Çalışması: Klavyeden bir kullanıcı adı ve şifre isteyen basit ATM uygulamasının bir parçasını aşağıdaki
+	açıklamalara göre yazınız
+	Açıklamalar:
+	- Kullanıcı adı ve şifresi en fazla 3(üç) kez denenebilecektir.
 	
+	- Denemeler sırasında doğru giriş yapılırsa "GİRİŞ BAŞARILI", başarısız denemelerde "GİRİŞ BAŞARISIZ!...", 3. denemede de
+	başarısız olunursa "GİRİŞ BAŞARISIZ. ARTIK DENEME HAKKINIZ BİTTİ!..." mesajları verilecektir.
 	
-	print ve println metotlarının String parametreli overload'ları argüman olarak aldıkları String referansına ilişkin
-	(ya da mantıksal olarak yazıyı) yazıyı ekrana basarlar 
+	- Kullanıcı adı ve şifrenin doğruluğu program içerisinde belirlenen bir kullanıcı adı ve şifre ile yapılacaktır.
+	
+	- Uygulamayı genel düşünürek yazınız	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{	
-		String s;
+	{			
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
+			
+			if ("elma".equals(s))
+				break;
+			
+			System.out.printf("(%s)%n", s);
+			System.out.printf("(%s)%n", s.trim());
+		}
 		
-		s = "ankara";
-		
-		System.out.println(s);
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
