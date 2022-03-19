a7b7946d14a93352796daa123ad6822f79250540
Oğuz Karan <oguzkaran@csystem.org>
2022-03-19 14:29:27 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,35 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Klavyeden bir kullanıcı adı ve şifre isteyen basit ATM uygulamasının bir parçasını aşağıdaki
-	açıklamalara göre yazınız
-	Açıklamalar:
-	- Kullanıcı adı ve şifresi en fazla 3(üç) kez denenebilecektir.
-	
-	- Denemeler sırasında doğru giriş yapılırsa "GİRİŞ BAŞARILI", başarısız denemelerde "GİRİŞ BAŞARISIZ!...", 3. denemede de
-	başarısız olunursa "GİRİŞ BAŞARISIZ. ARTIK DENEME HAKKINIZ BİTTİ!..." mesajları verilecektir.
-	
-	- Kullanıcı adı ve şifrenin doğruluğu program içerisinde belirlenen bir kullanıcı adı ve şifre ile yapılacaktır.
-	
-	- Uygulamayı genel düşünürek yazınız	
+	Aşağıdaki örnekte println metodu birleştirilmiş yazıyı argüman olarak alır ve ekrana basar. Şüphesiz birleştirme
+	işlemini println yapamaz. Çünkü metot çağrılmadan önce argümana ilişkin ifade hesaplanır, elde edilen değer ile
+	metot çağrılır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{			
+	{	
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			if ("elma".equals(s))
-				break;
-			
-			System.out.printf("(%s)%n", s);
-			System.out.printf("(%s)%n", s.trim());
-		}
+		System.out.printf("Kişi sayısını giriniz:");
+		int count = Integer.parseInt(kb.nextLine());		
 		
-		System.out.println("Tekrar yapıyor musunuz?");
+		System.out.println("Toplam " + count + " kişi aşı oldu");
 	}
 }
 
