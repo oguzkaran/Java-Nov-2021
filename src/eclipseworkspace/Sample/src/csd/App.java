e5b867bcc78359b53afe4902e6e0b52a840f83fc
Oğuz Karan <oguzkaran@csystem.org>
2022-03-19 14:57:31 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -7,12 +7,7 @@ package csd;
 class App {
 	public static void main(String [] args) 
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.printf("Kişi sayısını giriniz:");
-		int count = Integer.parseInt(kb.nextLine());		
-		
-		System.out.println("Toplam " + count + " kişi aşı oldu");
+		ReverseTest.run()
 	}
 }
 
