ce25dbd64e2686625ec4cb79b475cd73d6639fb6
Oğuz Karan <oguzkaran@csystem.org>
2022-03-20 16:25:19 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,57 +1,30 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının sadece başındaki boşluk karakterlerini atan trimLeading ve 
-	sadece sonundaki boşluk karakterlerini atan trimTrailing isimli metotlarını yazınız ve aşağıdaki kod ile test ediniz.	
+	Genel olarak söylemek gerekirse Scanner'ın çalışma sistematiği dolayısıyla nextInt, nextLong ve nextDouble
+	gibi metotlar aynı Scanner nesnesine ilişkin nextLine metodu ile aynı akışta çağrılmamalıdır. Buradaki
+	durum istemden sisteme değişiklik gösterebilir. Bu sebeple özellike klavye işlemlerinde (stdin) nextLine
+	metodu dışındakileri biz tercih etmiyoruz. Programcı nextLine metodunun diğer metotlarla aynı nesne için çağrılmacağından
+	eminse nextLong, nextInt ve nextDouble metotlarını kullanabilir. Aşağıdaki programı çalıştırırak sonucu
+	gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{	
-		TrimLeadingTrailingTest.run();						
-	}
-}
-
-class TrimLeadingTrailingTest {
-	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
+			System.out.print("Öğrencinin numarasını giriniz:");
+			int no = Integer.parseInt(kb.nextLine());
 			
-			if ("elma".equals(s))
+			if (no == 0)
 				break;
 			
-			System.out.printf("(%s)%n", StringUtil.trimLeading(s));
-			System.out.printf("(%s)%n", StringUtil.trimTrailing(s));
+			System.out.print("Öğrencinin ismini giriniz:");
+			String name = kb.nextLine();
+			
+			System.out.printf("%d, %s%n", no, name);
 		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-
-class StringUtil {
-	public static String trimLeading(String s)
-	{
-		int i;
-		int len = s.length();		
-	
-		for (i = 0; Character.isWhitespace(s.charAt(i)) && i < len; ++i)
-			;
-		
-		return s.substring(i);		
-	}
-	
-	public static String trimTrailing(String s)
-	{
-		int i;	
-		
-		for (i = s.length() - 1; i >= 0 && Character.isWhitespace(s.charAt(i)); --i)
-			;
-		
-		return s.substring(0, i + 1);
 	}
 }
 
