b80d1a60bb91ee7c98e138f5fc4a84b3024e7c4c
Oğuz Karan <oguzkaran@csystem.org>
2022-03-26 14:37:07 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,12 +1,6 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı yazının Türkçe pangram olup olmadığını test eden isPangramTR ve İngilizce 
-	pangram olup olmadığını test eden isPangramEN isimli metotları yazınız ve aşağıdaki kod ile test ediniz.
-	Pangram: İçerisinde özel isim olmayab anlamlı ve ilgili dilin alfabesindeki tüm karakterleri içeren cümlelere denir.
-	
-	Örnekte özel isim ve anlamlı olma durumu dikkate alınmayacaktır
-	
-	İngilizce: The quick brown fox jumps over the lazy dog.
-	Türkçe: Pijamalı hasta yağız şoföre çabucak güvendi.
+	Yukarıdaki örnek için İngilizce alfabedeki karakterler karakter tablosında sıralı olduğundan isPangramEN metodu
+	aşağıdaki gibi de yazılabilir. Ancak yukarıdaki yaklaşım daha geneldir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -19,51 +13,36 @@ class App {
 
 class IsPangramTest {
 	public static void run()
-	{
-		IsPangramTRTest.run();
-		System.out.println("--------------------------------");
-		IsPangramENTest.run();
-		
+	{		
+		IsPangramENTest.run();		
 	}
 }
-
-
 
 class IsPangramENTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		System.out.print("Input a text:");
-		String s = kb.nextLine();
-		
-		System.out.println(StringUtil.isPangramTR(s) ? "Pangram" : "Not a Pangram");		
+		for (;;) {
+			System.out.print("Input a text:");
+			String s = kb.nextLine();
+			
+			if ("quit".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPangramEN(s) ? "Pangram" : "Not a Pangram");
+		}
 	}
 }
 
 
-
-class IsPangramTRTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("Bir yazı giriniz:");
-		String s = kb.nextLine();
-		
-		System.out.println(StringUtil.isPangramTR(s) ? "Pangram" : "Pangram değil");
-		
-	}
-}
-
-class StringUtil {
-	public static boolean isPangramTR(String s)
-	{
-		
-	}
-	
+class StringUtil {	
 	public static boolean isPangramEN(String s)
 	{
+		for (char c = 'a'; c <= 'z'; ++c)
+			if (!s.contains(Character.toLowerCase(c) + ""))
+				return false;
 		
-	}
+		return true;
+	}	
 }
