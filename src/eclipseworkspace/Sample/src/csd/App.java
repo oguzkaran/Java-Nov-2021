58b662089aaef982c1abd4bbd2e85b6b001c3543
Oğuz Karan <oguzkaran@csystem.org>
2022-03-26 15:21:18 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,48 +1,47 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Yukarıdaki örnek için İngilizce alfabedeki karakterler karakter tablosında sıralı olduğundan isPangramEN metodu
-	aşağıdaki gibi de yazılabilir. Ancak yukarıdaki yaklaşım daha geneldir
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir count değeri için count tane rasgele üretilmiş Türkçe
+	karakterlerden oluşan bir yazı döndüren getRandomTextTR ile count tane rasgele üretilmiş İnglizce karakterlerden oluşan 
+	bir yazı döndüren getRandomTextEN metotlarını yazınız ve aşağıdaki kod ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		IsPangramTest.run();
+		GetRandomTextTRENTest.run();
 	}
 }
 
-class IsPangramTest {
-	public static void run()
-	{		
-		IsPangramENTest.run();		
-	}
-}
-
-class IsPangramENTest {
+class GetRandomTextTRENTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
-			System.out.print("Input a text:");
-			String s = kb.nextLine();
+			System.out.print("Bir sayı giriniz:");
+			int count = Integer.parseInt(kb.nextLine());
 			
-			if ("quit".equals(s))
+			if (count <= 0)				
 				break;
 			
-			System.out.println(StringUtil.isPangramEN(s) ? "Pangram" : "Not a Pangram");
+			System.out.printf("Şifre:%s%n", StringUtil.getRandomTextTR(count));
+			System.out.printf("PAssword:%s%n", StringUtil.getRandomTextEN(count));
+			
 		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
 
 class StringUtil {	
-	public static boolean isPangramEN(String s)
+	public static String getRandomTextTR(int count)
 	{
-		for (char c = 'a'; c <= 'z'; ++c)
-			if (!s.contains(Character.toLowerCase(c) + ""))
-				return false;
 		
-		return true;
 	}	
+	
+	public static String getRandomTextEN(int count)
+	{
+		
+	}
 }
