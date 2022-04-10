29ba9ac9d6a76c6bf73d6cf4edd53c3ca97bff75
Oğuz Karan <oguzkaran@csystem.org>
2022-04-10 16:27:24 +0300

    Update

--- src/Sample/src/org/csystem/util/array/ArrayUtil.java
+++ src/Sample/src/org/csystem/util/array/ArrayUtil.java
@@ -73,7 +73,8 @@ public class ArrayUtil {
 
     public static void reverse(int [] a)
     {
-        //TODO:
+        for (int i = 0;i < a.length / 2; ++i)
+            swap(a, i, a.length - 1 - i);
     }
 
     public static void swap(int [] a, int i, int k)
