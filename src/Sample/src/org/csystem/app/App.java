193337915a504a3a00e2e1fe4e0f5e603e5b5fbd
Oğuz Karan <oguzkaran@csystem.org>
2022-04-23 16:25:01 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,48 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Kabarcık sıralama algoritmasında dizinin yan yana iki elemanı karşılaştırılır, duruma göre yer değiştirilir. Her
-	yinelemede en büyük daraltılmış dizinin sonuna gider. Böylece her yinelemede eskisinden bir geriye gitmek yeterli olur
+	Sınıf Çalışması: Sayısal loto kuponu üreten bir program tasarlayıp yazınız
+	(İleride daha iyisi yazılacaktır)
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import static org.csystem.util.array.ArrayUtil.*;
-
-import java.util.Random;
-import java.util.Scanner;
+import org.csystem.app.lotto.NumericLotteryApp;
 
 class App {
 	public static void main(String [] args)
 	{
-		BubbleSortTest.run();
+		NumericLotteryApp.run();
 	}	
 }
-
-class BubbleSortTest {
-	public static void run()
-	{
-		Random r = new Random();
-		Scanner kb = new Scanner(System.in);
-
-		for (;;) {
-			System.out.print("Dizinin eleman sayısını giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
-
-			if (n <= 0)
-				break;
-
-			int [] a =getRandomArray(r, n, 0, 99);
-
-			print(2, a);
-
-			boolean desc = r.nextBoolean();
-
-			bubbleSort(a, desc);
-
-			System.out.printf("%s sıralanmış dizi:%n", desc ? "Büyük küçüğe" : "Küçükten büyüğe");
-			print(2, a);
-		}
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-
-
--- src/Sample/src/org/csystem/util/array/ArrayUtil.java
+++ src/Sample/src/org/csystem/util/array/ArrayUtil.java
@@ -29,6 +29,42 @@ public class ArrayUtil {
                     swap(a, k, k + 1);
     }
 
+    public static void selectionSortAscending(int [] a)
+    {
+        int min, minIndex;
+
+        for (int i = 0; i < a.length - 1; ++i) {
+            min = a[i];
+            minIndex = i;
+
+            for (int k = i + 1; k < a.length; ++k)
+                if (a[k] < min) {
+                    min = a[k];
+                    minIndex = k;
+                }
+            a[minIndex] = a[i];
+            a[i] = min;
+        }
+    }
+
+    public static void selectionSortDescending(int [] a)
+    {
+        int max, maxIndex;
+
+        for (int i = 0; i < a.length - 1; ++i) {
+            max = a[i];
+            maxIndex = i;
+
+            for (int k = i + 1; k < a.length; ++k)
+                if (max < a[k]) {
+                    max = a[k];
+                    maxIndex = k;
+                }
+            a[maxIndex] = a[i];
+            a[i] = max;
+        }
+    }
+
     public static void bubbleSort(int [] a)
     {
         bubbleSort(a, false);
@@ -92,6 +128,28 @@ public class ArrayUtil {
         return getRandomArray(new Random(), count, min, bound);
     }
 
+    public static int max(int [] a)
+    {
+        int result = a[0];
+
+        for (int i = 1; i < a.length; ++i)
+            if (a[i] < result)
+                result = a[i];
+
+        return result;
+    }
+
+    public static int min(int [] a)
+    {
+        int result = a[0];
+
+        for (int i = 1; i < a.length; ++i)
+            if (result < a[i])
+                result = a[i];
+
+        return result;
+    }
+
     public static void print(int [] a)
     {
         print(1, a);
@@ -123,6 +181,19 @@ public class ArrayUtil {
     {
         for (int i = 0;i < a.length / 2; ++i)
             swap(a, i, a.length - 1 - i);
+    }
+
+    public static void selectionSort(int [] a)
+    {
+        selectionSort(a, false);
+    }
+
+    public static void selectionSort(int [] a, boolean descending)
+    {
+        if (descending)
+            selectionSortDescending(a);
+        else
+            selectionSortAscending(a);
     }
 
     public static void swap(int [] a, int i, int k)
