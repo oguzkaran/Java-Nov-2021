0fde23948f7f8f511e2b1574ab6a628526b064ad
Oğuz Karan <oguzkaran@csystem.org>
2022-05-22 14:28:51 +0300

    Update App.java

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -2,7 +2,7 @@
 	Aşağıdaki durumda içi boş ()'ler geçerlidir. Hiç parantez konmaması ile aynı anlamdadır. Aslında aşağıdaki kullanım
 	ve parantezsiz kullanım, sabitlere ilişkin nesneler yaratılırken "default ctor" çağrılsın anlamındadır. Anımsanacağı
 	gibi bir sınıfa hiç bir ctor yazılmamaışsa derleyici default ctor'u içi boş ve public olarak yazar. enum sınıflar
-	için içi boş ve erişilemez olorak yazar.   
+	için içi boş ve erişilemez olorak yazar.
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
