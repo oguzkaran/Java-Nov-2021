8a158233f5877c6b61cb17d1fd11d3c4d14c403c
Oğuz Karan <oguzkaran@csystem.org>
2022-05-29 16:23:14 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,32 +1,25 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Türemiş sınıfı nesnesi içerisinde kadar alan da bulunur. Yani türemiş sınıf nesnesi içerisinde taban sınıfın
-	non-static veri elemanları da vardır. Bu durumda nesnesel kapsama söz konusudur. Bu durumda türemiş
-	sınıf nesnesinin uzunluğu "en az, taban sınıf nesnesinin bellekte kapladığı alan + türemiş sınıfa eklenen non-static
-	veri elemanlarının toplam uzunluğu kadardır"
+	super ctor sentaksı adından da anlaşaılacağı gibi yalnızca ctor içerisinde kullanılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String[] args)
 	{
-		B x = new B();
-
-		x.a = 10;
-		x.b = 30;
-
-		System.out.printf("x.a = %d%n", x.a);
-		System.out.printf("x.b = %d%n", x.b);
+		A x = new A(10);
 	}
 }
 
 
-class B extends A {
-	public int b;
-	//...
-}
-
 class A {
-	public int a;
+	public A()
+	{
+		System.out.println("A.A()");
+	}
 
+	public A(int a)
+	{
+		System.out.println("A.A(int)");
+	}
 	//...
 }
