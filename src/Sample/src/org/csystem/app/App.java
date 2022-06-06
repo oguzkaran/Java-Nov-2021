660818cff261afd9efd96b1415be536a71485674
Oğuz Karan <oguzkaran@csystem.org>
2022-06-06 18:38:33 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,16 +1,39 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	CompanyApp uygulaması
-	Not: CompanyApp sınıfındaki getManager, getSalesManager, getWorker ve getProjectWorker metotları tamamen konuyu
-	anlatmak için demo olarak yazılmıştır
+	Aşağıdaki örnekte z'nin ve t'nin dinamik türü C olur
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.app.company.CompanyApp;
 
 class App {
 	public static void main(String[] args)
 	{
-		CompanyApp.run();
+		C x = new C();
+		B y;
+		A z;
+		A t;
+
+		y = x;
+
+		System.out.println(y.getClass().getName());
+
+		z = y;
+
+		System.out.println(z.getClass().getName());
+
+		t = z;
+
+		System.out.println(t.getClass().getName());
 	}
+}
+
+class C extends B {
+	//...
+}
+
+class B extends A {
+	//...
+}
+
+class A {
+	//...
 }
 
