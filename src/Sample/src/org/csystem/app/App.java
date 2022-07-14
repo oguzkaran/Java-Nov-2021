75c3aa360b836f0cff9c395b85404e55645e86b8
Oğuz Karan <oguzkaran@csystem.org>
2022-07-15 00:25:42 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,23 +1,39 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örnekte static veri elemanı olan x değişkenine sınıf ismi ile erişilerek atama yapılmıştır. setX metodunun
-	parametre ismi de x olduğundan sınıf ismi ile veri elemanına erişilmesi gerekir
+	Aşağıdaki örnekte c referansına null atanması derleyicinin "değer verilmeme ihrtimali olan bir değişkeni kullanıyorsun"
+	şeklindeki hata durumunu engellemek için kullanılmıştır. Şüphesiz program farklı şekillerde de yazılabilir. Durumu
+	göstermek için bu şekilde yazılmıştır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
+import org.csystem.util.math.geometry.Circle;
+
+import java.util.Scanner;
+
 class App {
-	public static void main(String [] args)
+	public static void main(String[] args)
 	{
-		Sample.setX(20);
+		Scanner kb = new Scanner(System.in);
 
-		System.out.printf("Sample.x = %d%n", Sample.x);
+		for (;;) {
+			System.out.print("Bir sayı giriniz:");
+			int a = Integer.parseInt(kb.nextLine());
+
+			if (a < 0 || a > 3) {
+				System.out.println("Geçersiz değer girdiniz!...");
+				continue;
+			}
+
+			Circle c = null;
+
+			switch (a) {
+				case 0 -> c = new Circle(0);
+				case 1 -> c = new Circle(-1);
+				case 2 -> c = new Circle(-128);
+				case 3 -> c = new Circle(127);
+			}
+
+			System.out.printf("r = %f, area = %f, circumference = %f%n", c.getRadius(), c.getArea(), c.getCircumference());
+		}
 	}
 }
 
-class Sample {
-	public static int x;
-
-	public static void setX(int x)
-	{
-		Sample.x = x;
-	}
-}
