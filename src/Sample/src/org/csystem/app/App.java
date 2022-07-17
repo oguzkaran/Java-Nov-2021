e49ea60ecbe3655ece469c3460071b1859025345
Oğuz Karan <oguzkaran@csystem.org>
2022-07-17 19:38:24 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,42 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Bir sınıfın ctor'u içerisinde o sınıfın başka ctor'unun çağrılması kodu yazılmışsa (this ctor) bu durumda çağıran için
-    non-static initializer kodu yerleştirilmez. Çünkü zaten çağrılan ctor'da ya olacaktır y da da o da başka bir ctor'u
-    çağırsa da sonuçta çalıştırılacak non-static blok olacaktır. Yani bir non-static blok nesne başına bir kez çalıştırılır
+	Polymorphism (çok biçimlilik): Programlamaya Biyoloji'den aktarılmıştır. Biyoloji'de çok biçimlilik şu şekilde
+	tanımlanabilir: "Farklı doku ya da organların evrim süreci içerisinde temel işlevleri aynı kalmak koşuluyla, bu işlevi
+	yerine getirme biçiminin değişebilmesidir".
+
+	1. Biyolojik Tanım:
+	2. Yazılım Mühendisliği Tanımı:
+	3. Aşağı seviyeli tanım:
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String[] args)
 	{
-		Sample s1 = new Sample();
-		System.out.println("---------------------------------------");
-		Sample s2 = new Sample(10);
-		System.out.println("---------------------------------------");
-		Sample s3 = new Sample(4.5);
+
 	}
 }
 
-class Sample {
-	{
-		System.out.println("non-static initializer");
-	}
-
-	public Sample()
-	{
-		this(Math.E);
-		System.out.println("I am a default ctor");
-	}
-
-	public Sample(int a)
-	{
-		this();
-		System.out.println("I am a ctor with parameter type: int");
-	}
-
-	public Sample(double a)
-	{
-		System.out.println("I am a ctor with parameter type: double");
-	}
-
-	//...
-}
