0b59653a3f83e751bc5c701549fece559432880a
Oğuz Karan <oguzkaran@csystem.org>
2022-08-05 00:29:57 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,12 +1,53 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	final metotlar override edilemez. Yani final metotlar sanal değildir. Bir metot override edilip yapılabilir
+	Aşağıdaki örnekte porseDouble metodunun fırlattığı exception nesnesi de yakalanabildiğinden akış sonlanmaz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Scanner;
 
 class App {
 	public static void main(String[] args)
 	{
+		try {
+			Scanner kb = new Scanner(System.in);
+			System.out.print("Bir sayı giriniz:");
+			double a = Double.parseDouble(kb.nextLine());
+			double result = MathUtil.log10(a);
 
+			System.out.printf("Result:%f%n", result);
+		}
+		catch (UndefinedException ex) {
+			System.out.println("Undefined");
+		}
+		catch (IndeterminateException ex) {
+			System.out.println("Indeterminate");
+		}
+		catch (NumberFormatException ex) {
+			System.out.println("Geçersiz noktalı sayı girdiniz!...");
+		}
+
+		System.out.println("Tekrar yapıyor musnuz?");
 	}
 }
 
+class MathUtil {
+	public static double log10(double a)
+	{
+		if (a == 0)
+			throw new UndefinedException();
+
+		if (a < 0)
+			throw new IndeterminateException();
+
+		return Math.log10(a);
+	}
+}
+
+class UndefinedException extends RuntimeException {
+	//...
+}
+
+
+class IndeterminateException extends RuntimeException {
+	//...
+}
