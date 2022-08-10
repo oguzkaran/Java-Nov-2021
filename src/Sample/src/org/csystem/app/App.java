5106f23a805d4f938c89317103f0b11c2bdfef88
Oğuz Karan <oguzkaran@csystem.org>
2022-08-11 00:33:05 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,53 +1,28 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örnekte porseDouble metodunun fırlattığı exception nesnesi de yakalanabildiğinden akış sonlanmaz
+	Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import java.util.Scanner;
 
 class App {
 	public static void main(String[] args)
 	{
-		try {
-			Scanner kb = new Scanner(System.in);
-			System.out.print("Bir sayı giriniz:");
-			double a = Double.parseDouble(kb.nextLine());
-			double result = MathUtil.log10(a);
-
-			System.out.printf("Result:%f%n", result);
-		}
-		catch (UndefinedException ex) {
-			System.out.println("Undefined");
-		}
-		catch (IndeterminateException ex) {
-			System.out.println("Indeterminate");
-		}
-		catch (NumberFormatException ex) {
-			System.out.println("Geçersiz noktalı sayı girdiniz!...");
-		}
-
-		System.out.println("Tekrar yapıyor musnuz?");
+		System.out.println(Sample.foo("csd")); //csdtry
 	}
 }
 
-class MathUtil {
-	public static double log10(double a)
+class Sample {
+	public static String foo(String str)
 	{
-		if (a == 0)
-			throw new UndefinedException();
+		String result = str;
 
-		if (a < 0)
-			throw new IndeterminateException();
+		try {
+			str += "try";
+		}
+		finally {
+			str += "finally";
+		}
 
-		return Math.log10(a);
+		return str;
 	}
 }
 
-class UndefinedException extends RuntimeException {
-	//...
-}
-
-
-class IndeterminateException extends RuntimeException {
-	//...
-}
