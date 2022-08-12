199624496ef050ab1955ddf068b12e2c8d0207db
Oğuz Karan <oguzkaran@csystem.org>
2022-08-12 23:36:14 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,28 +1,106 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örneği inceleyiniz
+	Java'da exception sınıfları kategori olarak iki gruba ayrılır: checked, unchecked
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Scanner;
 
 class App {
 	public static void main(String[] args)
 	{
-		System.out.println(Sample.foo("csd")); //csdtry
+		try {
+			LogarithmApp.run();
+		}
+		catch (WrapperException ex) {
+			System.out.println(ex.getMessage());
+		}
+		finally {
+			System.out.println("main: finally");
+		}
+
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
-class Sample {
-	public static String foo(String str)
+class LogarithmApp {
+	public static void run()
 	{
-		String result = str;
-
 		try {
-			str += "try";
+			Scanner kb = new Scanner(System.in);
+			System.out.print("Bir sayı giriniz:");
+			double a = Double.parseDouble(kb.nextLine());
+			double result = MathUtil.log10(a);
+
+			System.out.printf("Result:%f%n", result);
+		}
+		catch (NumberFormatException ex) {
+			throw new WrapperException("Invalid Value");
+		}
+		catch (MathResultException ex) {
+			throw new WrapperException("Problem occured", ex);
 		}
 		finally {
-			str += "finally";
+			System.out.println("LogarithmApp.run:: finally");
 		}
 
-		return str;
+		System.out.println("LogarithmApp.run ends!...");
+	}
+}
+
+
+class WrapperException extends RuntimeException {
+	public WrapperException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public WrapperException(String message)
+	{
+		this(message, null);
+	}
+
+	public String getMessage()
+	{
+		Throwable cause = getCause();
+
+		return String.format("Message:%s%s", super.getMessage(),
+				cause == null ? "" : String.format(", Cause:%s, Cause Message:%s", cause.getClass().getName(), cause.getMessage()));
+	}
+}
+
+class MathUtil {
+	public static double log10(double a)
+	{
+		if (a == 0)
+			throw new MathResultException("Undefined", MathStatus.NEGATIVE_INFINITY);
+
+		if (a < 0)
+			throw new MathResultException("Indeterminate", MathStatus.NAN);
+
+		return Math.log10(a);
+	}
+}
+
+enum MathStatus {NAN, POSITIVE_INFINITY, NEGATIVE_INFINITY, POSITIVE_ZERO, NEGATIVE_ZERO,}
+
+class MathResultException extends RuntimeException {
+	private final MathStatus m_mathStatus;
+
+	public MathResultException(String message, MathStatus mathStatus)
+	{
+		super(message);
+		m_mathStatus = mathStatus;
+	}
+
+	public MathStatus getMathStatus()
+	{
+		return m_mathStatus;
+	}
+
+	public String getMessage()
+	{
+		return String.format("Message:%s, Status:%s", super.getMessage(), m_mathStatus);
 	}
 }
 
