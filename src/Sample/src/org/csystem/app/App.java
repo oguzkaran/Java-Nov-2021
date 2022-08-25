04eb75666cbfe78d59f1548826fd0a20bc8c5392
Oğuz Karan <oguzkaran@csystem.org>
2022-08-25 06:02:02 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,32 +1,61 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir sınıf başka bir sınıftan türetilip istediği kadar arayüzü destekleyebilir. Bu durumda extends'in başta yazılması
-	zorunludur
+	Soyut bir tür ne zaman interface, ne zaman abstract sınıf yapılmalıdır? Programcı buna nasıl karar verecektir?
+	Önce interface düşünülmeli, interface özellikleri yazacağınız türü karşılamıyorsa (abstract) sınıf yazılmalıdır
+
+	interface yapılabildiği yerde abstract sınıf yerine interface yapılması, bu interface'i destekleyen bir sınıfın
+	başka bir sınıftan da türetilmesi olanağını sağlar. Aynı zamanda okunabilirlik/algılanabilirlik de artırılmış olur
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String[] args)
 	{
+		Mample m = new Mample();
+		A a = new A();
+		B b = new B();
 
+		m.doWorkForIX(a);
+		m.doWorkForIX(b);
 	}
 }
 
-class B extends A implements IX, IY, IZ {
-	//...
+class Mample {
+	public void doWorkForIX(IX ix)
+	{
+		//...
+		ix.foo();
+		ix.bar();
+	}
 }
 
-class A {
-	//...
+class B implements IX {
+	public void foo()
+	{
+		System.out.println("B.foo");
+	}
+}
+
+
+class A implements IX {
+	public void foo()
+	{
+		System.out.println("A.foo");
+	}
 }
 
 interface IX {
-	//...
-}
+	default void foo()
+	{
+		throw new UnsupportedOperationException();
+	}
 
-interface IY {
-	//...
-}
+	default void bar()
+	{
+		throw new UnsupportedOperationException();
+	}
 
-interface IZ {
-	//...
+	default void tar()
+	{
+		//...
+	}
 }
