f4568cfd1894761d221b0e8a294c0644e72d9655
Oğuz Karan <oguzkaran@csystem.org>
2022-09-07 00:30:11 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,38 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki 3 ve 4 numaralı çağrılarda açılım belirtilmiş olsa da tam uyumlu metotlar seçilir. Bu işlem generic
-	bir işin türe özgü olarak özelleştirilmesi durumlarında kullanılır
+	Pair sınıfı ve test kodu
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String[] args)
 	{
-		Sample.foo("ankara"); //1
-		Sample.foo(2); //2
-		Sample.<String>foo("ankara"); //3
-		Sample.<Integer>foo(34); //4
-		Sample.foo(3.4); //5
+
 	}
 }
 
-class Sample {
-	public static <T> void foo(T t)
-	{
-		System.out.println("foo(T)");
-	}
-
-	public static void foo(String s)
-	{
-		System.out.println("foo(String)");
-	}
-
-	public static void foo(int val)
-	{
-		System.out.println("foo(int)");
-	}
-
-	public static void foo(Integer val)
-	{
-		System.out.println("foo(Integer)");
-	}
-}
---
+++ src/Sample/src/org/csystem/util/tuple/Triple.java
@@ -0,0 +1,15 @@
+/*----------------------------------------------------------------
+	FILE		: Triple.java
+	AUTHOR		: Java-Nov-2021 Group
+	LAST UPDATE	: 06.09.2022
+
+	Immutable Value that represents Tuple<T1, T2, T3>
+
+	Copyleft (c) 1993 C and System Programmers Association
+	All Rights Free
+----------------------------------------------------------------*/
+package org.csystem.util.tuple;
+
+public class Triple {
+    //...
+}
