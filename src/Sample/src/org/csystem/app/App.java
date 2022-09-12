8366345a23803264880ba384eac70b7cb6733b01
Oğuz Karan <oguzkaran@csystem.org>
2022-09-13 00:04:07 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,12 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Pair sınıfı ve test kodu
+	Dosya İşlemleri:
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.util.collection.CSDArrayList;
+import org.csystem.util.console.Console;
+import org.csystem.util.tuple.Triple;
+
+import java.util.Random;
+
+import static org.csystem.util.string.StringUtil.getRandomTextTR;
 
 class App {
 	public static void main(String[] args)
 	{
+		CSDArrayList<Triple<String, String, String>> passwords = new CSDArrayList<>();
+		Random r = new Random();
+		int count = Console.readInt("Bir sayı giriniz:");
 
+		for (int i = 0; i < count; ++i)
+			passwords.add(Triple.of(getRandomTextTR(r, r.nextInt(5, 10)), getRandomTextTR(r, r.nextInt(5, 10)),
+					getRandomTextTR(r, r.nextInt(5, 10))));
+
+		int size = passwords.size();
+
+		for (int i = 0; i < size; ++i)
+			Console.writeLine(passwords.get(i));
 	}
 }
 
